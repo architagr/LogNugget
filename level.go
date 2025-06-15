74bdf9207f173141931cf17923bc9becdc9691f8
Archit Agarwal <architagr@gmail.com>
2025-06-15 21:40:28 +0530

    Implement log fields (#1)

---
+++ level.go
@@ -0,0 +1,44 @@
+package lognugget
+
+import (
+	"fmt"
+)
+
+type LogLevel int
+
+const (
+	LevelDebug LogLevel = 0
+	LevelInfo  LogLevel = 2
+	LevelWarn  LogLevel = 4
+	LevelError LogLevel = 8
+	LevelFatal LogLevel = 16
+)
+
+// String returns a name for the level.
+// If the level has a name, then that name
+// in uppercase is returned.
+// If the level is between named values, then
+// an integer is appended to the uppercased name.
+// Examples:
+//
+//	LevelWarn.String() => "WARN"
+//	(LevelInfo+2).String() => "INFO+2"
+func (l LogLevel) String() string {
+	str := func(base string, val LogLevel) string {
+		if val == 0 {
+			return base
+		}
+		return fmt.Sprintf("%s%+d", base, val)
+	}
+
+	switch {
+	case l < LevelInfo:
+		return str("DEBUG", l-LevelDebug)
+	case l < LevelWarn:
+		return str("INFO", l-LevelInfo)
+	case l < LevelError:
+		return str("WARN", l-LevelWarn)
+	default:
+		return str("ERROR", l-LevelError)
+	}
+}
--- makefile
+++ makefile
@@ -1,14 +1,21 @@
 .PHONY: fmt vet test test-coverage
+outDir := .out
+coverageFile := $(outDir)/coverage.txt
+coverageHTML := $(outDir)/coverage.html
 
 fmt:
 	gofmt -w .
 
 test:
-	go test ./...
+	go test ./... -race -v -covermode=atomic
 
 vet:
 	go vet ./...
 
 test-coverage:
-	go test ./... -coverprofile coverage.txt -covermode=atomic 
-	go tool cover -html=coverage.txt -o coverage.html
+	mkdir -p .out
+	go test ./... -coverprofile $(coverageFile) -covermode=atomic 
+	go tool cover -html=$(coverageFile) -o $(coverageHTML)
+	@echo "Coverage report generated at $(coverageHTML)"
+	@echo "Opening coverage report in browser..."
+	open -a "Safari" $(coverageHTML)
